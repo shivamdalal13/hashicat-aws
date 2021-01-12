9727c952e24cdf078d6046c8d7f9481c3112ea2b
shivamdalal13 <shivamdalal13@gmail.com>
2021-01-12 17:29:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -168,7 +168,7 @@ resource "null_resource" "configure-cat-app" {
       "sudo chown -R ubuntu:ubuntu /var/www/html",
       "chmod +x *.sh",
       "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
-      "echo helloworld"
+      "echo helloworld",
       "sudo apt -y install cowsay",
       "cowsay Mooooooooooo!",
     ]
