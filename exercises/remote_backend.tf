02b10d117d04cec2c0cb47110e3fdf23c3c7ce40
shivamdalal13 <shivamdalal13@gmail.com>
2021-01-13 17:34:46 +0000

    Added org name

--- exercises/remote_backend.tf
+++ exercises/remote_backend.tf
@@ -5,9 +5,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "YOURORGANIZATION"
+    organization = "shivamdalal-training"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-aws"
     }
   }
 }
